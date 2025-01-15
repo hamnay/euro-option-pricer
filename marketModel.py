6b171106e380f56e26e9a15317b3e99e801cc11c
hamnay <hamnay99@gmail.com>
2025-01-15 15:29:10 +0100

    Initial commit

---
+++ marketModel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 15 15:16:55 2025
+
+@author: hamza
+"""
+
+import numpy as np
+from abc import ABC
+
+
+
+class marketmodel(ABC) :
+    def __init__(self):
+        pass
+    
+
+
+
